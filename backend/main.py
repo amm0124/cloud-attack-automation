44b6b970fdeba2fff347fba67c5ae590f1f63cf5
amm0124 <amm0124@naver.com>
2025-05-14 18:09:36 +0900

    init : init be directory

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,16 @@
-print("main")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def hello():
+    return {"message": "hello world"}
