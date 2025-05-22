d7dbf0eab386fd8b3089c00a9fcced73fbab7377
amm0124 <amm0124@naver.com>
2025-05-22 22:40:44 +0900

    feat : BE websocket api

---
+++ backend/app/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from fastapi import APIRouter
+from attacks.collecting.github.router import router as github_collector_router
+from fastapi.responses import StreamingResponse
+from fastapi import HTTPException
+import os
+app = FastAPI()
+app.include_router(github_collector_router)
+
+
+REPORTS_DIR = "/Users/geonho/workspace/cloud-attack-automation/backend/app" # 보고서 저장 경로
+
+router = APIRouter()
+
+@router.get("/download/{filename}", summary="보고서 작성 후 다운로드 및 삭제")
+async def download_report(filename: str):
+    safe_filename = os.path.basename(filename)
+    file_path = os.path.join(REPORTS_DIR, safe_filename)
+    print("file_path", file_path)
+
+    if not os.path.exists(file_path):
+        raise HTTPException(status_code=404, detail="파일이 존재하지 않습니다.")
+
+    def file_streamer(path):
+        with open(path, mode="rb") as file:
+            yield from file
+        try:
+            os.remove(path)
+            print(f"다운로드 후 성공적으로 삭제됨: {path}")
+        except Exception as e:
+            print(f"삭제 실패: {e}")
+
+    return StreamingResponse(
+        file_streamer(file_path),
+        media_type="application/octet-stream",
+        headers={"Content-Disposition": f"attachment; filename={safe_filename}"}
+    )
+
+app.include_router(router)
